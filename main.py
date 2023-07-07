d64fd649660559b8dedeb655599a09ebc1992ff6
Luca <61653175+iu2frl@users.noreply.github.com>
2023-07-07 15:34:01 +0200

    Added whitelist verification

--- main.py
+++ main.py
@@ -6,37 +6,62 @@ import logging
 import threading
 import re
 
+# Specify logging leveò
+logging.basicConfig(level=logging.DEBUG)
+
 # Read API Token from environment variables
 BOT_TOKEN: str = os.environ.get('BOT_TOKEN')
+if (not BOT_TOKEN):
+    logging.critical("Input token is empty!")
+    raise Exception("Invalid BOT_TOKEN")
 if (len(BOT_TOKEN) < 10):
-    raise Exception("Input token is too short!")
+    logging.critical("Input token is too short!")
+    raise Exception("Invalid BOT_TOKEN")
 if (":" not in BOT_TOKEN):
-    raise Exception("Invalid input token format")
+    logging.critical("Invalid input token format")
+    raise Exception("Invalid BOT_TOKEN")
+# Generate bot object
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # Read allowed chat from environment variables
-BOT_WHITELIST: list[int] = os.environ.get('BOT_WHITELIST').strip().split(",")
-if (len(BOT_WHITELIST) == 0):
-    print("WARNING: No whitelist have been specified, this means that anybody can use this bot and you may get banned!")
-if (BOT_WHITELIST is not list[int]):
-    raise Exception("WARNING: Invalid whitelist format! Syntax: 12345,09876,...")
-bot = telebot.TeleBot(BOT_WHITELIST)
+if (not os.environ.get('BOT_WHITELIST')):
+    logging.warning("No whitelist have been specified, this means that anybody can use this bot and you may get banned!")
+    BOT_WHITELIST: list[int] = []
+else:
+    BOT_WHITELIST: list[int] = os.environ.get('BOT_WHITELIST').strip().split(",")
+    if (BOT_WHITELIST is not list[int]):
+        logging.critical("Invalid whitelist format! Syntax: 12345,09876,...")
+        raise Exception("Invalid BOT_WHITELIST")
 
 # Check if message comes from a valid source
 def CheckWhitelist(inputMessage: telebot.types.Message) -> bool:
-    if (len(BOT_WHITELIST) == 0)
+    logging.debug("Checking message from [" + str(inputMessage.chat.id) + "]")
+    if (len(BOT_WHITELIST) == 0):
+        logging.debug("No whitelist, accepting by default")
+        return True
+    elif (inputMessage.chat.id in BOT_WHITELIST):
+        logging.debug("Chat is in whitelist, accepting")
+        return True
+    else:
+        logging.debug("Ignoring message from user: [" + inputMessage.from_user.username + "]")
 
 # Handle non-text messages
 @bot.message_handler(func=lambda message: True, content_types=['audio', 'photo', 'voice', 'video', 'document', 'location', 'contact', 'sticker'])
 def default_command(inputMessage: telebot.types.Message):
-    bot.send_message(inputMessage, "Hi " + inputMessage.from_user.first_name + ",\nI'm very sorry but i have no idea on how to interact with this object")
+    if CheckWhitelist(inputMessage):
+        bot.send_message(inputMessage, "Hi " + inputMessage.from_user.first_name + ",\nI'm very sorry but i have no idea on how to interact with this object")
 
 # Handle AI command
-@bot.message_handler(commands=['ai'])
+@bot.message_handler(content_types=["text"], commands=['ai', 'bing'])
 def HandleAiMessage(inputMessage: telebot.types.Message):
-    # Create async thread to handle replies
-    thread = threading.Thread(target=ReplyAi, args=(inputMessage, ))
-    thread.start()
+    if CheckWhitelist(inputMessage):
+        # Check that the massage contains some text
+        if (len(inputMessage.text) <= 5):
+            bot.reply_to(inputMessage, "Hi " + inputMessage.from_user.first_name + ",\nplease give me some data to process, syntax is: `/ai [text to interact with]`")
+            return
+        # Create async thread to handle replies
+        thread = threading.Thread(target=ReplyAi, args=(inputMessage, ))
+        thread.start()
 
 # Create async reply
 def ReplyAi(inputMessage: telebot.types.Message):
@@ -68,14 +93,22 @@ def ReplyAi(inputMessage: telebot.types.Message):
     except:
         bot.edit_message_text(gptResponse, inputMessage.chat.id, newReply.id)
 
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+# Welcome new users
+@bot.message_handler(content_types=["text"], commands=['start', 'hello'])
+def send_welcome(inputMessage: telebot.types.Message):
+    if CheckWhitelist(inputMessage):
+        bot.reply_to(inputMessage, "Hello " + inputMessage.from_user.first_name + "\nHow can i assist you today?")
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message: telebot.types.Message):
-    bot.reply_to(message, message.text)
+# Give project informations
+@bot.message_handler(content_types=["text"], commands=['info'])
+def send_welcome(inputMessage: telebot.types.Message):
+    if CheckWhitelist(inputMessage):
+        bot.reply_to(inputMessage, "Hello " + inputMessage.from_user.first_name + "\nThis project is hosted on a GitHub repository, do you want to partecipate? Here's the link: https://github.com/iu2frl/YotaBot")
+
+# @bot.message_handler(func=lambda msg: True)
+# def echo_all(message: telebot.types.Message):
+#     bot.reply_to(message, message.text)
 
 if __name__ == "__main__":
-    print("Starting bot")
+    logging.info("Starting bot")
     bot.infinity_polling()
